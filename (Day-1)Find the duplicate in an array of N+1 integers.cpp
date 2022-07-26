975c483da22295f852d87d9274d2d71b74a0fd47
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2022-07-27 02:05:04 +0600

    Create (Day-1)Find the duplicate in an array of N+1 integers.cpp

---
+++ (Day-1)Find the duplicate in an array of N+1 integers.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/find-the-duplicate-number/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast = nums[0] ;
+        int slow = nums[0] ;
+        
+        do {
+            fast = nums[nums[fast]] ;
+            slow = nums[slow] ;
+            
+        } while(fast != slow) ;
+        
+        fast = nums[0] ;
+        
+        while(fast != slow){
+            fast = nums[fast] ;
+            slow = nums[slow] ;
+        }
+
+        
+        return slow ;
+    }
+};

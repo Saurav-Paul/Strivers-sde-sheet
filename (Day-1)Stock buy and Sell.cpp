3759a2cc75015fbebed93f440ef5e68955bc5ae2
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2022-07-27 02:01:40 +0600

    Create (Day-1)Stock buy and Sell.cpp

---
+++ (Day-1)Stock buy and Sell.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0 ;
+        int mn = prices[0];
+        for(int i = 1 ; i < prices.size() ; i++){
+            mn = min(mn , prices[i]) ;
+            profit = max(profit, prices[i] - mn);
+        }
+        return profit;
+    }
+};

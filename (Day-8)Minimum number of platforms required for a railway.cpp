13c6465ae9c9fa2b875c97968163f63da844078c
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2022-08-05 23:14:22 +0600

    Create (Day-8)Minimum number of platforms required for a railway.cpp

---
+++ (Day-8)Minimum number of platforms required for a railway.cpp
@@ -0,0 +1,53 @@
+//https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1#
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	int platform = 0;
+    	vector<int> station(2470,0) ;
+    	
+    	for(int i = 0 ; i < n ; i++){
+    	    for(int j = arr[i] ; j <= dep[i] ; j++){
+    	        station[j] ++ ;
+    	        platform = max(platform, station[j]) ;
+    	    }
+    	}
+    	
+    	return platform ;
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends

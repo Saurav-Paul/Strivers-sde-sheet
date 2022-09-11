313d626530e86b26ce6799e9453b98aebf936e5c
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2022-09-11 22:53:41 +0600

    Create (Day-23)Clone a graph (Not that easy as it looks).cpp

---
+++ (Day-23)Clone a graph (Not that easy as it looks).cpp
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/clone-graph/
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    map<Node*, Node*> mp ;
+    Node* cloneGraph(Node* node) {
+
+        if(node == NULL) return node;
+        
+        if(mp.find(node) == mp.end()){
+            Node* temp = new Node(node -> val,{}) ;
+            mp[node] = temp ;
+            
+            for(auto x : node->neighbors){
+                temp->neighbors.push_back( cloneGraph(x) );
+            }
+            
+            
+        }
+        
+        return mp[node] ;
+    }
+};
